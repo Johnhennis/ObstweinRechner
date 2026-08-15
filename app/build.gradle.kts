80acbf0294a2def1bbdaecbd978483ce84bc686c
Johnhennis <johannes28991@gmail.com>
2026-08-15 08:44:18 +0000

    Version auf 12 (1.11) erhoeht

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "app.johnhennis.obstweinrechner"
         minSdk = 26
         targetSdk = 36
-        versionCode = 11
-        versionName = "1.10"
+        versionCode = 12
+        versionName = "1.11"
     }
 
     signingConfigs {
